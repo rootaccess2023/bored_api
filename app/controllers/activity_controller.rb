716fcf1780a06b2835bc66e151c0a8881642a205
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-29 14:48:20 +0100

    feat: add random_activity method to ActivityController

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,2 +1,14 @@
 class ActivityController < ApplicationController
+  def random_activity
+    count = params[:count].to_i
+
+    url = URI("https://bored-api.appbrewery.com/random")
+    
+    data = []
+    count.times do
+      response = Net::HTTP.get_response(url)
+      activity = JSON.parse(response.body)
+      data << activity
+    end
+  end
 end
